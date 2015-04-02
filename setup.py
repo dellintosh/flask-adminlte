650888f5923c9f2292991b9b1bf9363124dcac64
FvD <fvd@southshield.net>
2015-04-02 11:34:58 -0600

    Update to AdminLTE 2.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
     name='Flask-AdminLTE',
-    version='1.3.0.0.1.dev1',
+    version='2.0.6',
     url='http://github.com/jkinred/flask-adminlte',
     license='BSD',
     author='Jonathan Kinred',
