fbdb34551e51a2d33f5f49cc0814de217031c5d7
Justus Luthy <jluthy@expansioncapitalgroup.com>
2016-04-14 08:54:40 -0500

    Adds sample "current_user".
    This is (obviously) a fake user object, but it should give you an idea
    of how the User model can be handled.  In our business, we are using
    Flask-Login and Flask-Stormpath to manage our user models, but feel free
    to define your own if you feel called to.

--- sample_application/__init__.py
+++ sample_application/__init__.py
@@ -1,22 +1,37 @@
+import dateutil.parser
+
 from flask import Flask, render_template
 from flask_adminlte import AdminLTE
+
+
+class User(object):
+    """
+    Example User object.  Based loosely off of Flask-Login's User model.
+    """
+    full_name = "John Doe"
+    avatar = "/static/img/user2-160x160.jpg"
+    created_at = dateutil.parser.parse("November 12, 2012")
 
 
 def create_app(configfile=None):
     app = Flask(__name__)
     AdminLTE(app)
 
+    # This is a placeholder user object.  In the real-world, this would
+    # probably get populated via ... something.
+    current_user = User()
+
     @app.route('/')
     def index():
-        return render_template('index.html')
+        return render_template('index.html', current_user=current_user)
 
     @app.route('/login')
     def login():
-        return render_template('login.html')
+        return render_template('login.html', current_user=current_user)
 
     @app.route('/lockscreen')
     def lockscreen():
-        return render_template('lockscreen.html')
+        return render_template('lockscreen.html', current_user=current_user)
 
     return app
 
